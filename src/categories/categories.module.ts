b15e751a24f25aa347f7eba5011d058f57bf24f1
wcuadrosuniandes <w.cuadrosr@uniandes.edu.co>
2025-11-19 20:17:22 -0500

    Fix_ajustar_category

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { Category } from './entities/category.entity';
+import { CategoryTranslation } from './entities/category-translation.entity';
+import { Language } from 'src/languages/entities/language.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category, CategoryTranslation, Language])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
-export class CategoriesModule {}
+export class CategoriesModule { }
